9398ae143bcaf2341ee6e9fc662c1925540bae52
Lee Tran <lee.tran@hjsfmail.org>
2022-04-30 15:50:21 -0500

    Update osxdict.rb

--- osxdict.rb
+++ osxdict.rb
@@ -5,6 +5,8 @@ class OsxDict < Formula
   head 'https://github.com/leettran/osxdict.git'
   url 'https://github.com/leettran/osxdict/archive/1.0.0.tar.gz'
   desc 'Command-line interface to Dictionary.app on OSX'
+  sha256 "346834333b3b24b3b058f6b3ee12054de9ed552c93a6532f12c96b9f2543ed15"
+  license "Unlicense"
 
   depends_on "cmake" => :build
 
